2e10a4a4e44cd39e7282b5e63ed2e5759279231d
kappa0923 <tom.kappa0923@gmail.com>
2019-08-25 00:57:34 +0900

    オーガナイザーとパートナー追加

--- src/views/DevFest2019.vue
+++ src/views/DevFest2019.vue
@@ -1,11 +1,7 @@
 <template>
   <v-content class="pa-0">
     <v-container fluid style="padding: 0">
-      <!-- <v-layout wrap align-center justify-center row fill-height> -->
-      <!-- <v-flex xs12 md10> -->
       <DevFestTop />
-      <!-- </v-flex> -->
-      <!-- </v-layout> -->
     </v-container>
 
     <v-container fluid style="background-color:#F9F9F9">
@@ -23,6 +19,14 @@
         </v-flex>
       </v-layout>
     </v-container>
+
+    <!-- <v-container fluid style="background-color:#F9F9F9">
+      <v-layout wrap align-center justify-center row fill-height>
+        <v-flex xs12 md10>
+          <DevFestSponsor />
+        </v-flex>
+      </v-layout>
+    </v-container> -->
 
     <v-container fluid class="my-4">
       <v-layout wrap align-center justify-center row fill-height>
@@ -49,6 +53,7 @@ import DevFestAbout from "@/components/devfest2019/DevFestAbout";
 import DevFestPhoto from "@/components/devfest2019/DevFestPhoto";
 import DevFestJoin from "@/components/devfest2019/DevFestJoin";
 import DevFestOrganizer from "@/components/devfest2019/DevFestOrganizer";
+// import DevFestSponsor from "@/components/devfest2019/DevFestSponsor";
 
 export default {
   components: {
@@ -57,7 +62,8 @@ export default {
     DevFestAbout,
     DevFestPhoto,
     DevFestJoin,
-    DevFestOrganizer
+    DevFestOrganizer,
+    // DevFestSponsor
   }
 };
 </script>
