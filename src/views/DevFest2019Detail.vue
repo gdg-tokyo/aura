0cfaedd1af7c59fc15c7f2b3f6b7c1957126922b
Shoko Okochi <fly.child1017@gmail.com>
2019-11-12 01:35:23 +0900

    call CloudFunctions feature

--- src/views/DevFest2019Detail.vue
+++ src/views/DevFest2019Detail.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div>
-            <h2>{{speakerID}}</h2>
+            <h2>{{detail.name}}</h2>
             <p>
                 {{detail.profile}}
             </p>
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     created() {
         this.getDetail()
@@ -22,11 +24,11 @@ export default {
     },
     methods: {
         getDetail: function () {
-            this.speakerID = this.$route.params.id
-            // const speakerID = this.$route.params.id
-            // const baseUrl = 'https://us-central1-gdg-tokyo-website.cloudfunctions.net/api/speaker/'
-            // axios.get(baseUrl + speakerID).then(response => {this.detail = response})
-            this.detail = {"id":"emma.haruka.iwao","name":"岩尾 エマ はるか","position":"Developer Advocate","englishName":"Developer Advocate","twitter":"https://twitter.com/Yuryu","profileImage":"https://storage.googleapis.com/gdgtokyo_img/devfest19/speaker/emma_haruka_iwao","profile":"組み込みソフトウェア開発者、Site Reliability Engineerなどを経て2015年より現職。現在はGoogleのシアトルオフィスにて、Google Cloud PlatformのDeveloper Advocateとしてアプリケーション開発者に技術の魅力を伝える仕事をしている。大学では高性能計算および分散ファイルシステムを研究し、その経験を元にクラウド環境上での高性能計算を専門にしている。趣味は旅行とゲーム。"}
+            const speakerID = this.$route.params.id
+            const baseUrl = 'https://us-central1-gdg-tokyo-website.cloudfunctions.net/api/speaker/'
+            axios.get(baseUrl + speakerID).then(response => {
+                this.detail = response.data
+            })
         },
     }
 }
