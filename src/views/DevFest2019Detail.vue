0ac504b1bad2cf19c7b1a1046857e2a929303c43
Shoko Okochi <fly.child1017@gmail.com>
2019-11-24 16:33:43 +0900

    bug fix

--- src/views/DevFest2019Detail.vue
+++ src/views/DevFest2019Detail.vue
@@ -1,41 +1,36 @@
 <template>
   <v-container>
-    <v-layout v-if="detail!=''">
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>{{detail.name}}</v-card-title>
-        <v-card-text>{{detail.position}}</v-card-text>
-        <v-divider></v-divider>
-        <v-layout wrap row fill-height align-center class="pa-4">
-          <v-flex xs12 sm4 md3 lg2 class="pa-2">
-            <v-responsive :aspect-ratio="1/1">
-              <v-avatar size="100%">
-                <v-img :src="getStorageUrl(detail.profileImage)" :lazy-src="getStorageUrl(detail.profileImage)" v-on="on">
-                  <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
-                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                  </v-layout>
-                </v-img>
-              </v-avatar>
-            </v-responsive>
-          </v-flex>
-          <v-flex xs12 sm8 md9 lg10 class="pa-2">
-            <v-card-text>
-              <h2>Profile</h2>
-              <p style="white-space:pre-wrap; word-wrap:break-word;">{{detail.profile}}</p>
-            </v-card-text>
-          </v-flex>
-        </v-layout>
-        <v-layout wrap row fill-height align-center class="pa-4" v-if="(detail.sessionTitle).length>0">
+    <v-card>
+      <v-card-title class="headline grey lighten-2" primary-title>{{detail.name}}</v-card-title>
+      <v-card-text>{{detail.position}}</v-card-text>
+      <v-divider></v-divider>
+      <v-layout wrap row fill-height align-center class="pa-4">
+        <v-flex xs12 sm4 md3 lg2 class="pa-2">
+          <v-responsive :aspect-ratio="1/1">
+            <v-avatar size="100%">
+              <v-img :src="getStorageUrl(detail.profileImage)" :lazy-src="getStorageUrl(detail.profileImage)" v-on="on">
+                <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
+                  <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                </v-layout>
+              </v-img>
+            </v-avatar>
+          </v-responsive>
+        </v-flex>
+        <v-flex xs12 sm8 md9 lg10 class="pa-2">
           <v-card-text>
-            <h2>講演概要</h2>
-            <h3>{{detail.sessionTitle}}</h3>
-            <p>{{detail.sessionDetail}}</p>
+            <h2>Profile</h2>
+            <p style="white-space:pre-wrap; word-wrap:break-word;">{{detail.profile}}</p>
           </v-card-text>
-        </v-layout>
-      </v-card>
-    </v-layout>
-    <v-layout v-if="detail==''">
-      <h2>調整中</h2>
-    </v-layout>
+        </v-flex>
+      </v-layout>
+      <v-layout wrap row fill-height align-center class="pa-4" v-if="(detail.sessionTitle).length>0">
+        <v-card-text>
+          <h2>講演概要</h2>
+          <h3>{{detail.sessionTitle}}</h3>
+          <p>{{detail.sessionDetail}}</p>
+        </v-card-text>
+      </v-layout>
+    </v-card>
   </v-container>
 </template>
 
@@ -60,17 +55,13 @@ export default {
     methods: {
         getDetail: function () {
             const speakerID = this.$route.params.id
-            if speakerID != null{
-              const baseUrl = '/api/speaker/'
-              axios.get(baseUrl + speakerID).then(response => {
-                  this.detail = response.data
-                  this.speakerName = this.detail.name
-                  this.speakerProfile = response.data.profile
-                  this.setMeta()
-              })
-            } else {
-              this.detail = ''
-            }
+            const baseUrl = '/api/speaker/'
+            axios.get(baseUrl + speakerID).then(response => {
+                this.detail = response.data
+                this.speakerName = this.detail.name
+                this.speakerProfile = response.data.profile
+                this.setMeta()
+            })
         },
         setMeta: function () {
             var title = this.speakerName + ' | GDG DevFest Tokyo 2019'
