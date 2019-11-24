85cc65bf066b0fb9a94da2bafef6f67dc9947ea8
Shoko Okochi <fly.child1017@gmail.com>
2019-11-24 21:34:42 +0900

    add link to schedule

--- src/components/devfest2019/schedule/Schedule.vue
+++ src/components/devfest2019/schedule/Schedule.vue
@@ -18,43 +18,14 @@
               <div v-for="(sdata,key) in sessionsData" :key="key">
                 <div v-if="obj == sdata.id" class="py-3 pl-3">
                   <v-layout row wrap>
-                    <router-link style="cursor: pointer" tag="session" v-bind:to="{ name : 'devfest_session_detail', params : { id: sdata.speakers[0] }}">
-                      <v-flex xs12 class="pl-3" :style="{ 'border-left':  getBorderColor(sdata.place)}" >
-                        <p class="google-font mb-0" style="font-size:130%">{{sdata.title}}</p>
-                        <p style="font-size:100%" v-if="sdata.speakerName.length>0">{{sdata.speakerName}}</p>
-                        <v-chip
-                          class="ml-0"
-                          :color="sdata.tag.color"
-                          flat
-                          v-if="sdata.timeDuration<60"
-                          small
-                        ></v-chip>
-                        {{ sdata.tag.name }}
-                        <v-chip
-                          class="white--text ml-0"
-                          color="black"
-                          label
-                          v-if="sdata.timeDuration<60"
-                          small
-                        ><v-icon x-small>mdi-av-timer</v-icon> {{ sdata.timeDuration }} min</v-chip>
-                        <v-chip class="ml-1 mr-1" dark :color="getColor(sdata.place)" small label>
-                          <v-icon x-small>mdi-google-maps</v-icon>
-                          {{sdata.place}}
-                        </v-chip>
-                        <span v-for="sd in sdata.speakers" :key="sd">
-                          <span v-for="(sp,i) in speakerData" :key="i">
-                            <v-chip
-                              color="teal"
-                              label
-                              small
-                              outlined
-                              v-if="sp.id == sd"
-                              class="mr-1 my-2"
-                            ><v-icon x-small>mdi-account-outline</v-icon>{{ sp.name }}</v-chip>
-                          </span>
-                        </span>
-                      </v-flex>
-                    </router-link>
+                    <div v-if="hasInfo(sdata.speakers[0])">
+                      <router-link style="cursor: pointer" tag="session" v-bind:to="{ name : 'devfest_session_detail', params : { id: sdata.speakers[0] }}">
+                        <ScheduleRow :sdata="sdata" />
+                      </router-link>
+                    </div>
+                    <div v-else>
+                      <ScheduleRow :sdata="sdata" />
+                    </div>
                   </v-layout>
                 </div>
               </div>
@@ -94,47 +65,55 @@
 </style>
 
 <script>
+import ScheduleRow from '@/components/devfest2019/schedule/ScheduleRow'
 import DevfestInfo from "@/assets/data/devfest2019.json";
 import ScheduleData from "@/assets/data/devfest2019schedule.json";
 import SessionData from "@/assets/data/devfest2019session.json";
+
 export default {
   data: () => ({
     speakerData: DevfestInfo.Speakers,
     scheduleData: ScheduleData, // FIXME:スピーカー情報をAPIのレスポンスから取り出す
     sessionsData: SessionData
   }),
+  components:{
+    ScheduleRow
+  },
   methods: {
     getBorderColor(data){
       switch(data){
         case '講堂ホール':
-          return '5px solid #1A73E8'
+        return '5px solid #1A73E8'
         case 'B101':
-          return '5px solid #EA4335'
+        return '5px solid #EA4335'
         case 'B102':
-          return '5px solid #FBBC04'
+        return '5px solid #FBBC04'
         case 'B201':
-          return '5px solid #34AB53'
+        return '5px solid #34AB53'
         case 'B202':
-          return '5px solid purple'
+        return '5px solid purple'
         default:
-          return '5px solid orange'
+        return '5px solid orange'
       }
     },
     getColor(data){
       switch(data){
         case '講堂ホール':
-          return '#1A73E8'
+        return '#1A73E8'
         case 'B101':
-          return '#EA4335'
+        return '#EA4335'
         case 'B102':
-          return '#FBBC04'
+        return '#FBBC04'
         case 'B201':
-          return '#34AB53'
+        return '#34AB53'
         case 'B202':
-          return 'purple'
+        return 'purple'
         default:
-          return 'orange'
+        return 'orange'
       }
+    },
+    hasInfo(data){
+      return data.length > 0
     }
   },
   filters: {
