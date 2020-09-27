991aaa72defa6623ce71a0f5947cf7da62aa8f73
sokume <sokume2106@gmail.com>
2020-09-27 14:47:36 +0900

    GDG Okayama の情報を追加

--- src/components/devfest2020/DevFestAbout.vue
+++ src/components/devfest2020/DevFestAbout.vue
@@ -9,7 +9,7 @@
       <v-flex xs12 md9 lg9 class="pa-2 text-xs-left">
         <p
           class="google-font"
-        >DevFest は、Google Developer Group (GDG) コミュニティによって世界各地で開かれるデベロッパー向けイベントです。東京では、Android、Google Cloud Platform（GCP）、Web、Firebase、Machine Learning （ML）、Assistant、Flutter、Goといった様々な技術の最新情報や現場でのノウハウを学べるコミュニティイベントとして開催しています。<br>GDG Fukushima, GDG Kyoto, GDG Kyusyu, GDG Nagoya, GDG Nara, GDG Osaka, GDG Shinsyu, GDG Tokyo, GDG Yamaguchi, WTM Tokyo, WTM Kyoto</p>
+        >DevFest は、Google Developer Group (GDG) コミュニティによって世界各地で開かれるデベロッパー向けイベントです。東京では、Android、Google Cloud Platform（GCP）、Web、Firebase、Machine Learning （ML）、Assistant、Flutter、Goといった様々な技術の最新情報や現場でのノウハウを学べるコミュニティイベントとして開催しています。<br>GDG Fukushima, GDG Kyoto, GDG Kyusyu, GDG Nagoya, GDG Nara, GDG Okayama, GDG Osaka, GDG Shinsyu, GDG Tokyo, GDG Yamaguchi, WTM Tokyo, WTM Kyoto</p>
         <p
           class="google-font"
         >The DevFest, or 'Developer Festival', is a technical conference for developers. It is aimed at students, professionals or simply curious techies.</p>
