0ffb651b37f880058c940f91c8bfa748976e5d7e
kappa0923 <tom.kappa0923@gmail.com>
2020-10-04 00:40:28 +0900

    Update timetable.

--- src/components/devfest2020/schedule/Schedule.vue
+++ src/components/devfest2020/schedule/Schedule.vue
@@ -14,6 +14,7 @@
                 <v-img
                     :src="sdata.profileImage"
                     :lazy-src="sdata.profileImage"
+                    :aspect-ratio="1/1"
                 >
                   <v-layout
                       slot="placeholder"
